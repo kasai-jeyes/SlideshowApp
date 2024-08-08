40aa2b22effe39a4d8ab84f0045a489cb4d5ff8f
kasai-jeyes <kasai_s@jeyes.co.jp>
2024-08-08 17:01:55 +0900

    進むボタンと戻るボタンを追加した。

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet weak var susumuButton: UIButton!
+    @IBOutlet weak var modoruButton: UIButton!
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var imageView: UIImageView!
     
@@ -32,16 +35,35 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func nextButton(_ sender: Any) {
+        if (timer == nil) {
+            // 次の画像へ
+            changeImage()
+        }
+    }
+    
+    @IBAction func backButton(_ sender: Any) {
+        if (timer == nil) {
+            // 次の画像へ
+            backImage()
+        }
+    }
+    
     @IBAction func slideShowButton(_ sender: Any) {
         // 再生中か停止しているかを判定
         if (timer == nil) {
             // 再生時の処理を実装
             
             // タイマーをセットする
-            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(changeImage), userInfo: nil, repeats: true)
+            timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(changeImage), userInfo: nil, repeats: true)
             
             // ボタンの名前を停止に変える
             startButton.setTitle("停止", for: .normal)
+            
+            // 進むボタンと戻るボタンを無効化
+            susumuButton.isEnabled = false
+            modoruButton.isEnabled = false
+            
             
         } else {
             // 停止時の処理を実装
@@ -54,6 +76,9 @@ class ViewController: UIViewController {
             // ボタンの名前を再生に直しておく
             startButton.setTitle("再生", for: .normal)
             
+            // 進むボタンと戻るボタンを有効化
+            susumuButton.isEnabled = true
+            modoruButton.isEnabled = true
         }
     }
     
@@ -65,6 +90,19 @@ class ViewController: UIViewController {
         if (nowIndex == imageArray.count) {
             // indexを一番最初の数字に戻す
             nowIndex = 0
+        }
+        // indexの画像をstoryboardの画像にセットする
+        imageView.image = imageArray[nowIndex]
+    }
+    
+    @objc func backImage() {
+        // indexを減らして表示する画像を切り替える
+        nowIndex -= 1
+
+        // indexが最初の数字になったら
+        if (nowIndex == 0) {
+            // indexを一番最後の数字に戻す
+            nowIndex = imageArray.count
         }
         // indexの画像をstoryboardの画像にセットする
         imageView.image = imageArray[nowIndex]
